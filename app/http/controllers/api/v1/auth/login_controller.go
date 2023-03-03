947601b8c87b38a7666ffd4bf7db06b216825731
ccl <811687790@qq.com>
2023-03-03 15:43:09 +0800

    使用密码登录

--- app/http/controllers/api/v1/auth/login_controller.go
+++ app/http/controllers/api/v1/auth/login_controller.go
@@ -29,3 +29,20 @@ func (lc *LoginController) LoginByPhone(c *gin.Context) {
 		})
 	}
 }
+
+func (lc *LoginController) LoginByPassword(c *gin.Context) {
+	request := requests.LoginByPasswordRequest{}
+	if ok := requests.Validate(c, &request, requests.LoginByPassword); !ok {
+		return
+	}
+
+	user, err := auth.Attempt(request.LoginID, request.Password)
+	if err != nil {
+		response.Unauthorized(c, "账号不存在或密码不正确")
+	} else {
+		token := jwt.NewJWT().IssueToken(user.GetStringID(), user.Name)
+		response.JSON(c, gin.H{
+			"token": token,
+		})
+	}
+}
