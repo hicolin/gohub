3ee892d844fbab538dae08272849671686617fa9
ccl <811687790@qq.com>
2023-03-21 14:36:42 +0800

    当前用户接口

--- routes/api.go
+++ routes/api.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"github.com/gin-gonic/gin"
+	controllers "gohub/app/http/controllers/api/v1"
 	"gohub/app/http/controllers/api/v1/auth"
 	"gohub/app/http/middlewares"
 )
@@ -32,5 +33,8 @@ func RegisterAPIRoutes(r *gin.Engine) {
 			authGroup.POST("/password-reset/using-phone", middlewares.GuestJWT(), pwc.ResetByPhone)
 			authGroup.POST("/password-reset/using-email", middlewares.GuestJWT(), pwc.ResetByEmail)
 		}
+
+		uc := new(controllers.UsersController)
+		v1.GET("/user", middlewares.AuthJWT(), uc.CurrentUser)
 	}
 }
