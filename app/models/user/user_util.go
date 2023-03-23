9e1e6284b281fae69dcaf88d3f2d7c03571135e4
ccl <811687790@qq.com>
2023-03-23 16:36:18 +0800

    用户列表分页

--- app/models/user/user_util.go
+++ app/models/user/user_util.go
@@ -1,6 +1,11 @@
 package user
 
-import "gohub/pkg/database"
+import (
+	"github.com/gin-gonic/gin"
+	"gohub/pkg/app"
+	"gohub/pkg/database"
+	"gohub/pkg/paginator"
+)
 
 func IsEmailExist(email string) bool {
 	var count int64
@@ -40,5 +45,16 @@ func GetByEmail(email string) (userModel User) {
 
 func All() (users []User) {
 	database.DB.Find(&users)
+	return
+}
+
+func Paginate(c *gin.Context, PerPage int) (users []User, paging paginator.Paging) {
+	paging = paginator.Paginate(
+		c,
+		database.DB.Model(User{}),
+		&users,
+		app.V1URL(database.TableName(&User{})),
+		PerPage,
+	)
 	return
 }
